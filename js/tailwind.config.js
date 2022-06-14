d8cd9acb7e33c3c292e1c22a980f7bddf912e2e4
KAKAOVX <>
2022-06-14 09:31:10 +0900

    신규 css 기능 - cascade layers

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {},
+      screens: {
+        sm: '465px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1640px',
+      },
+    },
+  };
